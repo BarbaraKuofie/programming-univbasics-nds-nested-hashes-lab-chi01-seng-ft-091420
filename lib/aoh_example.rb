686e9af0c9b28941234dcd92e1dc503de9d969aa
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-29 05:04:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,6 +16,11 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   [
+    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+    JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+    KATE_G = { name:  "Kate Gompert", occupation: "None" },
+    BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
@@ -25,4 +30,7 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key] = new_value
+end 
+aoh 
 end
